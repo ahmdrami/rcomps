58c56f90dd1ce0361ab9197bd3de191f2df18b26
Ahmad Rami <ahmadramen@gmail.com>
2020-04-12 23:23:28 +0100

    updated theme.ts

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,34 @@
+/** @jsx jsx */
+import { Fragment, FunctionComponent } from 'react'
+import { jsx } from 'theme-ui'
+type ButtonProps = {
+  onClick?: any
+  variant: 'secondary' | 'primary'
+}
+/**
+ * An example button
+ **/
+const Button: FunctionComponent<ButtonProps> = ({
+  variant = 'primary',
+  children,
+  ...props
+}) => {
+  return (
+    <Fragment>
+      <button
+        sx={{
+          variant: `buttons.${variant}`,
+          padding: 3,
+          borderColor: 'currentColor',
+          borderRadius: 2,
+          fontSize: [12, 14, 16],
+        }}
+        {...props}
+      >
+        {children}
+      </button>
+    </Fragment>
+  )
+}
+
+export default Button
